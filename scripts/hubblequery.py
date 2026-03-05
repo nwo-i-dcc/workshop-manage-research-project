b71a3fb53a49cc7fb7c886b332d7d9f94e59e61e
Jelle de Plaa <j.de.plaa@sron.nl>
2026-03-05 09:51:29 +0100

    Updated hubblequery.py to fix changes in astroquery output

--- scripts/hubblequery.py
+++ scripts/hubblequery.py
@@ -5,7 +5,7 @@ from astropy import constants as const
 from astropy.table import QTable
 
 
-def hubblequery():
+def main():
     """This script retrieves the latest velocities and distances for the objects that
     Edwin Hubble used to find the famous Hubble universe expansion law in 1929."""
 
@@ -22,19 +22,19 @@ def hubblequery():
 
     # Convert redshifts to velocities and convert MAIN_ID to strings
     c = const.c.to('km/s')
-    for i in range(hubble_data['RVZ_RADVEL'].size):
+    for i in range(hubble_data['rvz_radvel'].size):
         # If the datatype is redshift (z), then multiply with c to obtain velocity in km/s
-        if hubble_data['RVZ_TYPE'][i] == 'z':
-            hubble_data['RVZ_RADVEL'][i] = hubble_data['RVZ_RADVEL'][i] * c.value
+        if hubble_data['rvz_type'][i] == 'z':
+            hubble_data['rvz_radvel'][i] = hubble_data['rvz_radvel'][i] * c.value
         # Convert the column with source IDs from object to strings (otherwise conversion to FITS fails)
-        hubble_data['MAIN_ID'][i] = str(hubble_data['MAIN_ID'][i])
+        hubble_data['main_id'][i] = str(hubble_data['main_id'][i])
 
     # Check that we get some data back
-    print(hubble_data['MAIN_ID', 'Distance_distance', 'Distance_unit', 'RVZ_RADVEL'])
+    print(hubble_data['main_id', 'mesdistance.dist', 'mesdistance.unit', 'rvz_radvel'])
 
     # Create a new astropy table with the required columns
-    newtable = QTable([hubble_data['MAIN_ID'], hubble_data['Distance_distance'],
-                      hubble_data['Distance_unit'], hubble_data['RVZ_RADVEL']],
+    newtable = QTable([hubble_data['main_id'], hubble_data['mesdistance.dist'],
+                      hubble_data['mesdistance.unit'].astype('U4'), hubble_data['rvz_radvel']],
                       names=('Object', 'Distance', 'Distance_unit', 'Velocity'),
                       dtype=('U15', 'f8', 'U4', 'f8'))
 
@@ -42,4 +42,4 @@ def hubblequery():
     newtable.write('hubble_data.fits', format='fits')
 
 if __name__ == "__main__":
-    hubblequery()
+    main()
